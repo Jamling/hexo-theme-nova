10ffcab11b2a212de3580a8f1c92eb50f7272389
Jamling <li.jamling@gmail.com>
2016-06-21 19:54:37 +0800

    update fancybox

--- source/js/script.js
+++ source/js/script.js
@@ -2,22 +2,20 @@
   // Caption
   $('.article').each(function(i){
     $(this).find('img').each(function(){
-      /*if (!$(this).hasClass('img-responsive')) {
-      $(this).addClass('img-responsive')
-      }*/
-      if ($(this).parent().hasClass('fancybox')) return;
+      if ($(this).parent().hasClass('fancybox-button')) return;
 
       var alt = this.alt;
 
       if (alt) $(this).after('<span class="funcybox-caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox-button"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    $(this).find('.fancybox-button').each(function(j){
+      $(this).attr('rel', 'fancybox-button');
     });
     
+    // table 
     $(this).find('table').each(function(){
       if (!$(this).hasClass('table-bordered')) {
         $(this).addClass('table');
@@ -27,7 +25,35 @@
   });
 
   if ($.fancybox){
-    $('.fancybox').fancybox();
+    $('.fancybox-button').fancybox({
+      helpers:{
+        title: {
+          type: 'over'
+        },
+        buttons:{},
+        overlay : {
+            css : {
+                'background' : 'rgba(58, 42, 45, 0.5)'
+            }
+        }
+      },
+      afterShow: function () {
+            /* Add watermark */
+            $('<div class="watermark"></div>')
+                .bind("contextmenu", function (e) {
+                    return false; /* Disables right click */
+                })
+                .prependTo( $.fancybox.inner );  
+
+        $(".fancybox-title").wrapInner('<div />').show();
+        
+        $(".fancybox-wrap").hover(function() {
+            $(".fancybox-title").show();
+        }, function() {
+            $(".fancybox-title").hide();
+        });                
+        }
+    });
   }
 
   // highlight
